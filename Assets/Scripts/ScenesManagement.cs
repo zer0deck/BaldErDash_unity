16fcbf28ee81ddfad3d9dc5755d68bc9a899d1e3
zer0deck <zer0deck@icloud.com>
2022-10-03 17:25:39 +0300

    made Scene transition

--- Assets/Scripts/ScenesManagement.cs
+++ Assets/Scripts/ScenesManagement.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class ScenesManagement : MonoBehaviour
 {
@@ -14,12 +16,22 @@ public class ScenesManagement : MonoBehaviour
         private set;
     }
 
-    public GameObject PauseMenu, Pause, Controls, PauseButton;
+    public GameObject PauseMenu, Pause, Controls, PauseButton, Transitor;
     private Button PB;
+    private Animator transitionAnimator;
+    public TextMeshProUGUI Loading_info;
+    public Image Loading_bar;
+    private RectTransform LoadingBar_Rect;
+    private bool playerDataSaved = false;
     // Legacy: SettingsMenu, CheckMenuOut, GameOverMenu;
 
     private void Start() {
         PB = PauseButton.GetComponent<Button>();
+        transitionAnimator = Transitor.GetComponent<Animator>();
+        LoadingBar_Rect = Loading_bar.GetComponent<RectTransform>();
+        LoadingBar_Rect.offsetMax = new Vector2 (0,LoadingBar_Rect.offsetMax[1]);
+        Loading_info.text = "Loading completed";
+        transitionAnimator.SetTrigger("Break");
     }
     public void PauseButtonPressed()
     {
@@ -74,6 +86,7 @@ public class ScenesManagement : MonoBehaviour
 
     public void MainMenuButtonPressed()
     {
+        Time.timeScale = 1f;
         LoadScene("MainMenu");
     }
     public void ToMapButtonPressed()
@@ -89,15 +102,27 @@ public class ScenesManagement : MonoBehaviour
     private async UniTask LoadSceneCoroutine(string sceneName)
     {
         Scene exitScene = SceneManager.GetActiveScene();
+        LoadingBar_Rect.offsetMax = new Vector2 (-1200,LoadingBar_Rect.offsetMax[1]);
+        Loading_info.text = "Loading... 10%";
+        transitionAnimator.SetTrigger("Use");
         LoadSceneParameters param = new LoadSceneParameters(LoadSceneMode.Single);
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, param);
         asyncLoad.allowSceneActivation = false;
-        await SavePlayerDataCoroutine();
-        asyncLoad.allowSceneActivation = true;
-        while (!asyncLoad.isDone || asyncLoad.progress < 0.9f)
-        {
+        SavePlayerData();
+        await UniTask.WaitUntil(() => playerDataSaved == false);
+        
+        float right = 1200;
+        do {
+            Loading_info.text = $"Loading... {asyncLoad.progress*100}%";
+            right = Mathf.Lerp(right, asyncLoad.progress, Time.deltaTime * 2);
+            LoadingBar_Rect.offsetMax = new Vector2 (-right,LoadingBar_Rect.offsetMax[1]);
             await UniTask.Yield();
-        }
+        } while (asyncLoad.progress < 0.89);
+        LoadingBar_Rect.offsetMax = new Vector2 (0,LoadingBar_Rect.offsetMax[1]);
+        Loading_info.text = "Loading completed";
+        await UniTask.Delay(TimeSpan.FromSeconds(1), ignoreTimeScale: true);
+        // await UniTask.WaitUntil(() => transitionFinished==true);
+        asyncLoad.allowSceneActivation = true;
         await SceneManager.UnloadSceneAsync(exitScene);
     }
     public void Quit() {
@@ -112,6 +137,8 @@ public class ScenesManagement : MonoBehaviour
         Debug.Log("Saving player data");
         await UniTask.DelayFrame(60);
         Debug.Log("Player data saved");
+        playerDataSaved = true;
+        playerDataSaved = false;
     }
 
 }
