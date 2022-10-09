2814e6a8c01114bdffcb1ca921f707ac58148b1c
kanjohonda <voronmisha04@gmail.com>
2022-10-09 17:40:00 +0300

    made ManaSystem.cs

---
+++ Assets/Scripts/UI/ManaSystem.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaSystem : MonoBehaviour
+{
+ 
+    [SerializeField] private GameObject _linebar;
+    private RectTransform bar;
+    public static ManaSystem instance { get; private set;}
+
+    private void Start() 
+    {
+        bar = _linebar.GetComponent<RectTransform>();
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            return;
+        }
+        Destroy(this.gameObject);
+    }
+
+    private const float SHIFT = 232;
+    public void SetValue(float value)
+    {
+        if (value < 0) value = 0f;
+        if (value > 1) value = 1;
+        StartCoroutine(ChangeMana(value));
+    }
+    
+    IEnumerator ChangeMana(float value)
+    {   
+        while (bar.offsetMax[0] > (- ( SHIFT * (1 - value) )))
+        {
+            bar.offsetMax = new Vector2( Mathf.Lerp(bar.offsetMax[0], - ( SHIFT * (1 - value) ), 3f*Time.deltaTime), bar.offsetMax[1] );
+            yield return new WaitForEndOfFrame();
+        }   
+
+    }           
+
+}   
